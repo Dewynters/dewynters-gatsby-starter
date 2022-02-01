8210da5b38d25d881af42f6d4d7b1e677d93d768
Nick East <nickeast1998@gmail.com>
2022-02-01 17:18:44 +0000

    svgs moved to own folder + simple react light box added

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /\.svg$/
+          include:  /svgs/
         },
       },
     },
