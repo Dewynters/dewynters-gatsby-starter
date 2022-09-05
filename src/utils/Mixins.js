77e6eb5679e9e5f5f6b007ddae523ba30d91f1af
Nick <nickeast1998@gmail.com>
2022-09-05 12:22:15 +0100

    latest

--- src/utils/Mixins.js
+++ src/utils/Mixins.js
@@ -1,7 +1,13 @@
 import { css } from 'styled-components'
 
-const Mixins = {
-    
-}
-
-export default Mixins
+export const Inset = () => css`
+    @supports (inset: 0) {
+        inset: 0;
+    }
+    @supports not (inset: 0) {
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+    }
+`;
